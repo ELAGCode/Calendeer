f01005b24d3657b65cfac776ff87c498453bd16d
ELAGCode <agnumcode@gmail.com>
2024-08-29 13:35:19 -0500

    Added edit modal styles.

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -7,9 +7,9 @@ export type calendarType = {
 };
 
 export type calendarCellType = {
-  day: number,
-  month: number,
-  year: number,
+  day: number;
+  month: number;
+  year: number;
   hasSchedule: boolean;
   hasNote: boolean;
   note: {
@@ -32,10 +32,25 @@ const month = new Date().getMonth();
 const year = new Date().getFullYear();
 
 const Calendar: FC<calendarType> = ({ calendarData }) => {
-
   const [modalOpen, setModalOpen] = useState<boolean>(false);
   const [calendarIndex, setCalendarIndex] = useState<number | undefined>();
   const [currentMonthDays, setCurrentMonthDays] = useState<number>();
+
+  // create cells of empty objects
+  const calendarCellObjects = Array(currentMonthDays).fill({});
+  const calendarCellObjectsFilled = calendarCellObjects.map((_cell, idx) => {
+    return {
+      //0 based index -> calendar based index (1 based)
+      day: idx + 1,
+      month: 8,
+      year: 2024,
+      hasSchedule: false,
+      hasNote: true,
+      note: { text: "test note" },
+      //if schedule is true then schedule will be read and processed
+      schedule: { date: "", details: {} },
+    };
+  });
 
   useEffect(() => {
     setCurrentMonthDays(daysInMonth(year, month));
@@ -50,35 +65,19 @@ const Calendar: FC<calendarType> = ({ calendarData }) => {
     currentMonthDays,
   }) => {
     if (currentMonthDays) {
-      // create cells of empty objects
-      const calendarCellObjects = Array(currentMonthDays).fill({});
-      const calendarCellObjectsFilled = calendarCellObjects.map(
-        (_cell, idx) => {
-          return {
-            //0 based index -> calendar based index (1 based)
-            day: idx + 1,
-            month: 8,
-            year: 2024,
-            hasSchedule: false,
-            hasNote: true,
-            note: { text: "test note" },
-            //if schedule is true then schedule will be read and processed
-            schedule: { date: "", details: {} },
-          };
-        }
-      );
-
       return (
         <div>
           {calendarCellObjectsFilled.map((cell: calendarCellType, index) => {
             return (
               <div
                 onClick={(e) => handleCalendarCellClick(e, index)}
-                key={index}>
+                key={index}
+              >
                 {/* {cell?.hasNote ? "hasNote:true " : "hasNote:false "}
                 {cell?.hasSchedule ? "hasSchedule:true " : "hasSchedule:false "}
                 {cell?.note?.text} */}
-                {cell?.day}{cell.hasNote ? "*" : ""}
+                {cell?.day}
+                {cell.hasNote ? "*" : ""}
               </div>
             );
           })}
@@ -87,13 +86,37 @@ const Calendar: FC<calendarType> = ({ calendarData }) => {
     } else return <></>;
   };
 
-
-
   return (
     <div>
       <CalendarCellComponentFactory currentMonthDays={currentMonthDays} />
       {modalOpen ? (
-        <EditCellModal setModalOpen={setModalOpen} cellData={mockData[1]} />
+        <>
+          {/* modal shadow */}
+          <span
+            style={{
+              backgroundColor: "black",
+              position: "absolute",
+              opacity: 0.5,
+              padding: "100vh",
+              left: "0",
+              transform: "translateY(-100vh)",
+              overflow: "hidden",
+            }}
+          ></span>
+          <span
+            // edit cell container style
+            style={{
+              left: "30%",
+              right: "30%",
+              position: "absolute",
+              transform: "translateY(-30em)",
+              backgroundColor: "darkgray",
+              overflow: "hidden",
+            }}
+          >
+            <EditCellModal setModalOpen={setModalOpen} cellData={calendarCellObjectsFilled[calendarIndex!]} />
+          </span>
+        </>
       ) : (
         <></>
       )}
--- src/components/EditCellModal.tsx
+++ src/components/EditCellModal.tsx
@@ -1,21 +1,46 @@
-import {Dispatch, FC, SetStateAction} from 'react'
-import { calendarCellType } from './Calendar';
+import { Dispatch, FC, SetStateAction } from "react";
+import { calendarCellType } from "./Calendar";
 
 export type editCellType = {
-  cellData: undefined | calendarCellType,
-  setModalOpen: Dispatch<SetStateAction<boolean>>
+  cellData: undefined | calendarCellType;
+  setModalOpen: Dispatch<SetStateAction<boolean>>;
 };
 
-const EditCellModal: FC<editCellType> = ({cellData, setModalOpen}) => {
+const EditCellModal: FC<editCellType> = ({ cellData, setModalOpen }) => {
   return (
-    <>
-    <span> {cellData?.day} </span>
-    <span> {cellData?.month} </span>
-    <span> {cellData?.year} </span>
-    <span> {cellData?.note.text} </span>
-    <span onClick={() => setModalOpen(false)}> [Close]</span>
-    </>
-  )
-}
+    <span>
+      <div>
+        <h3>
+          Edit cell {cellData?.day}
+          <span
+            style={{
+              fontSize: "12px",
+              position: "absolute",
+              marginLeft: "30%",
+              marginTop: "5px",
+            }}
+            onClick={() => setModalOpen(false)}
+          >
+            Close [X]
+          </span>
+        </h3>
+      </div>
+      <hr />
 
-export default EditCellModal
+      <div>
+        <br />
+        <span> {cellData?.month} </span>
+        <span> {cellData?.day} </span>
+        <span> {cellData?.year} </span>
+      </div>
+      <br />
+      <div>
+        <textarea placeholder="day details..."></textarea>
+      </div>
+      <span> {cellData?.note.text} </span>
+      <span></span>
+    </span>
+  );
+};
+
+export default EditCellModal;
